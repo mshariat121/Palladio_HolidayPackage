12f0862941b18d8cd770952cdb74439e2352aafb
mshariat121 <m.shariat.62@gmail.com>
2025-06-05 12:27:48 -0700

    Setup file added

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+        This function will return the list of requirements.
+    """
+    requirement_lst:List[str] = []
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+
+                ## ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found!')
+    
+    return requirement_lst
+
+setup(
+    name = 'Palladio_HolidayPackage',
+    version = '0.0.1',
+    author = 'Mohammad Shariatmadari',
+    author_email = 'm.shariat.62@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
+
+
+
+
+
